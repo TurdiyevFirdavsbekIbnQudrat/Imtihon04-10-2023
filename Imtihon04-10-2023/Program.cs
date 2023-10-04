953c62287bafc5835ad7f56c04eb058d673047b7
TurdiyevFirdavsbekIbnQudrat <f1rdavsb3kturdiy3vibnqudrat@gmail.com>
2023-10-04 20:48:54 +0500

    Добавьте файлы проекта.

---
+++ Imtihon04-10-2023/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Imtihon04_10_2023;
+using Newtonsoft.Json;
+
+using(HttpClient client = new HttpClient())
+{
+    string Base_Url= "https://jsonplaceholder.typicode.com/posts";
+    HttpResponseMessage responce = await client.GetAsync(Base_Url);
+    string JsonData = await responce.Content.ReadAsStringAsync();
+    List<User> Data = JsonConvert.DeserializeObject<List<User>>(JsonData);
+    foreach(var j in Data)
+    {
+        Console.WriteLine("userId- {0}",j.userId);
+        Console.WriteLine("id- {0}", j.id);
+        Console.WriteLine("tiitle-{0}", j.title);
+        Console.WriteLine("body-{0}", j.body);
+        Console.WriteLine("//////////////////////////////////////");
+    }
+}
---
+++ Imtihon04-10-2023/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imtihon04_10_2023
+{
+    public class User
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+}
